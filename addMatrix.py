135f818fcf38fcf7711e680b3914b2ba6018b98e
jayasankar-shyam <jayasankar06shyam@gmail.com>
2022-06-02 21:48:08 +0530

    add

---
+++ addMatrix.py
@@ -0,0 +1,8 @@
+import numpy as mat
+mat1=mat.array([[1,2,3],[4,5,6]])
+mat2=mat.array([[7,8,9],[10,11,12]])
+print("First matrix : \n",mat1)
+print("Second matrix : \n",mat2)
+sum=mat.add(mat1,mat2)
+print("Sum : \n",sum)
+
